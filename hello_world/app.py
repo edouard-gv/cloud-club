735f865c7999ec61c1d7bb42307e29dd33ac3db3
Edouard <edouard@gomez-vaez.com>
2019-11-04 23:23:15 +0100

    Testing a second deployement from github

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Arolla",
+            "message": "Hello Arolla!",
             # "location": ip.text.replace("\n", "")
         }),
     }
